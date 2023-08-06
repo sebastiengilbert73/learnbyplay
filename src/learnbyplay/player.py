fc6278e0bb88fe944485cd8ebfa15ded511d1231
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2023-08-06 11:21:34 -0400

    .\src\learnbyplay\player.py: PositionRegressionPlayer: Adds field self.acts_as_opponent; ChooseMove(): Supports temperature=0; PredictReturns(): if the player acts as the opponent, swap the state tensor

--- src/learnbyplay/player.py
+++ src/learnbyplay/player.py
@@ -35,21 +35,35 @@ class ConsolePlayer(Player):
         return chosen_move
 
 class PositionRegressionPlayer(Player):
-    def __init__(self, identifier, neural_net, temperature=1.0, flatten_state=True):
+    def __init__(self, identifier, neural_net, temperature=1.0, flatten_state=True,
+                 acts_as_opponent=False):
         super(PositionRegressionPlayer, self).__init__(identifier)
         self.neural_net = neural_net
         self.neural_net.eval()
         self.temperature = temperature
         self.flatten_state = flatten_state
         self.device = next(self.neural_net.parameters()).device
+        self.acts_as_opponent = acts_as_opponent
 
-    def ChooseMove(self, authority: learnbyplay.games.rules.Authority, state_tsr: torch.Tensor) -> str:
+    def ChooseMove(self, authority: learnbyplay.games.rules.Authority,
+                   state_tsr: torch.Tensor) -> str:
+        #if must_swap_agent_and_opponent:
+        #    state_tsr = authority.SwapAgentAndOpponent(state_tsr)
         move_predicted_return_list = self.PredictReturns(state_tsr, authority)
         legal_moves = []
         corresponding_predicted_returns = []
+        highest_predicted_return = -2.0
+        champion_move = None
         for move, predicted_return in move_predicted_return_list:
             legal_moves.append(move)
             corresponding_predicted_returns.append(predicted_return)
+            if predicted_return > highest_predicted_return:
+                highest_predicted_return = predicted_return
+                champion_move = move
+
+        if self.temperature <= 0:  # Zero temperature: return the greedy best move
+            return champion_move
+
         corresponding_predicted_temperature_returns_tsr = torch.tensor(corresponding_predicted_returns)/self.temperature
         corresponding_probabilities_tsr = torch.nn.functional.softmax(corresponding_predicted_temperature_returns_tsr, dim=0)
         random_nbr = random.random()
@@ -68,6 +82,8 @@ class PositionRegressionPlayer(Player):
                 state_tsr, move, self.identifier
             )
             candidate_state_tsr = candidate_state_tsr.float().to(self.device)
+            if self.acts_as_opponent:
+                candidate_state_tsr = authority.SwapAgentAndOpponent(candidate_state_tsr)
             if self.flatten_state:
                 candidate_state_tsr = candidate_state_tsr.view(-1)
             predicted_return = self.neural_net(candidate_state_tsr.unsqueeze(0)).squeeze().item()
