a708db01fda555d99c975fa8f51813698983ffe2
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2023-08-06 11:19:23 -0400

    .\src\learnbyplay\games\tictactoe.py: Adds method SwapAgentAndOpponent()

--- src/learnbyplay/games/tictactoe.py
+++ src/learnbyplay/games/tictactoe.py
@@ -83,12 +83,11 @@ class TicTacToe(learnbyplay.games.rules.Authority):
     def StateTensorShape(self):
         return (2, 3, 3)
 
-    """def SwapPosition(self, state_tsr):
+    def SwapAgentAndOpponent(self, state_tsr):
         swapped_state_tsr = torch.zeros_like(state_tsr)
         swapped_state_tsr[0, :, :] = state_tsr[1, :, :]
         swapped_state_tsr[1, :, :] = state_tsr[0, :, :]
         return swapped_state_tsr
-    """
 
     def ThereIsALine(self, state_tsr, channel):
         there_is_a_line = False
