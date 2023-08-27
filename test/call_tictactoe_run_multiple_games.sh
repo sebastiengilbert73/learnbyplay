2f048cd52b1f324b2c1c4417847de63f33e774a0
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2023-08-27 09:45:33 -0400

    test/call_tictactoe_run_multiple_games.sh: Adds --agentLookAheadDepth

--- test/call_tictactoe_run_multiple_games.sh
+++ test/call_tictactoe_run_multiple_games.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 python tictactoe_run_multiple_games.py \
 	--agentArchitecture=SaintAndre_1024 \
-	--agentNeuralNetworkFilepath="..\experiments\output_tictactoe_train_agent_level20\SaintAndre_1024.pth" \
+	--agentNeuralNetworkFilepath="..\experiments\output_tictactoe_train_agent_level5\SaintAndre_1024.pth" \
 	--agentTemperature=0 \
+	--agentLookAheadDepth=2 \
 	--numberOfGames=1000 \
 	
