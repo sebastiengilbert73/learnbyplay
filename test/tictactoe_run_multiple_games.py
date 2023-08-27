15675d3daadc1e5b8d559d07a22fd971c14d14fe
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2023-08-27 09:46:32 -0400

    test/tictactoe_run_multiple_games.py: Adds --agentLookAheadDepth, --opponentLookAheadDepth

--- test/tictactoe_run_multiple_games.py
+++ test/tictactoe_run_multiple_games.py
@@ -15,9 +15,11 @@ def main(
     agentArchitecture,
     agentNeuralNetworkFilepath,
     agentTemperature,
+    agentLookAheadDepth,
     opponentArchitecture,
     opponentNeuralNetworkFilepath,
     opponentTemperature,
+    opponentLookAheadDepth,
     numberOfGames,
     useCpu,
     randomSeed
@@ -60,7 +62,8 @@ def main(
             neural_net=neural_net,
             temperature=agentTemperature,
             flatten_state=True,
-            acts_as_opponent=False
+            acts_as_opponent=False,
+            look_ahead_depth=agentLookAheadDepth
         )
     opponent = learnbyplay.player.RandomPlayer(opponent_identifier)
     if opponentNeuralNetworkFilepath is not None:
@@ -87,7 +90,8 @@ def main(
             neural_net=opponent_neural_net,
             temperature=opponentTemperature,
             flatten_state=True,
-            acts_as_opponent=True
+            acts_as_opponent=True,
+            look_ahead_depth=opponentLookAheadDepth
         )
 
 
@@ -106,12 +110,14 @@ if __name__ == '__main__':
     parser.add_argument('--agentArchitecture', help="In case of a neural network, the architecture. Default: 'SaintAndre_512'", default='SaintAndre_512')
     parser.add_argument('--agentNeuralNetworkFilepath', help="The filepath to the agent neural network. Default: 'None'", default='None')
     parser.add_argument('--agentTemperature', help="The agent temperature. Default: 1.0", type=float, default=1.0)
+    parser.add_argument('--agentLookAheadDepth', help="The agent search depth. Default: 2", type=int, default=2)
     parser.add_argument('--opponentArchitecture',
                         help="In case of a neural network, the architecture. Default: 'SaintAndre_512'",
                         default='SaintAndre_512')
     parser.add_argument('--opponentNeuralNetworkFilepath',
                         help="The filepath to the opponent neural network. Default: 'None'", default='None')
     parser.add_argument('--opponentTemperature', help="The opponent temperature. Default: 1.0", type=float, default=1.0)
+    parser.add_argument('--opponentLookAheadDepth', help="The opponent search depth. Default: 2", type=int, default=2)
     parser.add_argument('--numberOfGames', help="The number of games played. Default: 1000", type=int, default=1000)
     parser.add_argument('--useCpu', help="Force using CPU", action='store_true')
     parser.add_argument('--randomSeed', help="The random seed. Default: 0", type=int, default=0)
@@ -125,9 +131,11 @@ if __name__ == '__main__':
         args.agentArchitecture,
         args.agentNeuralNetworkFilepath,
         args.agentTemperature,
+        args.agentLookAheadDepth,
         args.opponentArchitecture,
         args.opponentNeuralNetworkFilepath,
         args.opponentTemperature,
+        args.opponentLookAheadDepth,
         args.numberOfGames,
         args.useCpu,
         args.randomSeed
